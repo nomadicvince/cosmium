6254267821c933ada254d4d72a8062821581e71b
Vincent Moore <cosmiqwarrior@gmail.com>
2018-10-17 12:43:37 -0400

    Update cosmiumSale.sol

--- contracts/cosmiumSale.sol
+++ contracts/cosmiumSale.sol
@@ -1,39 +1,106 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.25;
 import "./cosmium.sol";
 
-contract CosmiumSale {
+contract CosmiumICO is Cosmium {
+    address public admin;
+    address public deposit;
 
-    address admin;
-    Cosmium public tokenContract;
-    uint256 public tokenPrice;
-    uint256 public tokensSold;
-
-    event Sell(address _buyer, uint256 _amount);
-
-    constructor(Cosmium _tokenContract, uint256 _tokenPrice) public {
-        admin = msg.sender;
-        tokenContract = _tokenContract;
-        tokenPrice = _tokenPrice;
-    }
-
-    function multiply(uint x, uint y) internal pure returns (uint z) {
-        require(y == 0 || (z = x * y) / y == x);
-    }
-
-    function buyTokens(uint256 _numberOfTokens) public payable {
-        require(msg.value == multiply(_numberOfTokens, tokenPrice));
-        require(tokenContract.balanceOf(this) >= _numberOfTokens);
-        require(tokenContract.transfer(msg.sender, _numberOfTokens));
-
-        tokensSold += _numberOfTokens;
-
-        emit Sell(msg.sender, _numberOfTokens);
-    }
-
-    function endSale() public {
+    //token price in wei: 1 CMQ = 0.001 Ether | 1 Ether = 1000 CMQ
+    uint tokenPrice = 1000000000000000;
+    
+    //300 Ether
+    uint hardCap = 300000000000000000000;
+    
+    uint public raisedAmount;
+    
+    uint public saleStart = now;
+    uint public saleEnd = now + 604800; // one week
+    uint public coinTradeStart = saleEnd + 604800; //trading begins one week after ICO sale 
+    
+    uint public maxInvest = 5000000000000000000;
+    uint public minInvest = 1000000000000000;
+    
+    enum State{beforeStart, running, afterEnd, halted}
+    State public icoState;
+    
+    modifier onlyAdmin() {
         require(msg.sender == admin);
-        require(tokenContract.transfer(admin, tokenContract.balanceOf(this)));
-
-        admin.transfer(address(this).balance);
+        _;
+    }
+    
+    event Invest(address investor, uint value, uint tokens);
+    
+    constructor(address _deposit) public {
+        deposit = _deposit;
+        admin = msg.sender;
+        icoState = State.beforeStart;
+    }
+    
+    //emergency stop
+    function halt() public onlyAdmin {
+        icoState = State.halted;
+    }
+    
+    //restart ICO 
+    function restart() public onlyAdmin {
+        icoState = State.running;
+    }
+    
+    function changeDepositAddress(address newDeposit) public onlyAdmin {
+        deposit = newDeposit;
+    }
+    
+    function getCurrentState() public view returns(State) {
+        if(icoState == State.halted) { 
+            return State.halted;
+        } else if(block.timestamp < saleStart) {
+            return State.beforeStart;
+        } else if(block.timestamp >= saleStart && block.timestamp <= saleEnd) {
+            return State.running;
+        } else {
+            return State.afterEnd;
+        }
+    }
+    
+    function invest() payable public returns(bool) {
+        icoState = getCurrentState();
+        require(icoState == State.running);
+        
+        require(msg.value >= minInvest && msg.value <= maxInvest);
+        
+        uint tokens = msg.value / tokenPrice;
+        
+        require(raisedAmount + msg.value <= hardCap);
+        raisedAmount += msg.value;
+        
+        balances[msg.sender] += tokens;
+        balances[founder] -= tokens;
+        
+        deposit.transfer(msg.value);
+        
+        emit Invest(msg.sender, msg.value, tokens);
+        
+        return true;
+    }
+    
+    function burn() public returns(bool) {
+        icoState = getCurrentState();
+        require(icoState == State.afterEnd);
+        balances[founder] = 0;
+    }
+    
+    function() payable public {
+        invest();
+    }
+    
+    function transfer(address to, uint tokens) public returns(bool) {
+        require(block.timestamp > coinTradeStart);
+        super.transfer(to, tokens);
+    }
+    
+    function transferFrom(address _from, address _to, uint _value) public returns(bool) {
+        require(block.timestamp > coinTradeStart);
+        super.transferFrom(_from, _to, _value);
     }
 }
+
